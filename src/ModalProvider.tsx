bda3ec09f76eba8ca61fe07e3627151f14467b1b
imagine10255 <imagine10255@gmail.com>
2024-07-18 14:43:33 +0800

    fix eslint import sort

--- src/ModalProvider.tsx
+++ src/ModalProvider.tsx
@@ -1,5 +1,5 @@
-import React, {createContext, useContext} from 'react';
 import Logger from '@acrool/js-logger';
+import React, {createContext, useContext} from 'react';
 
 
 interface IContextProps {
